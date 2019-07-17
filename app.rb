6a0dd3ed9d85ca55b2ee02199e1d936e4ee5517a
Azraf Chowdhury <achowdhur@ltu.edu>
2019-07-17 03:35:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,12 +3,19 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
-  get "reversename/:name" do
+  get 'reversename/:name' do
 
   end
 
-  get "square/:number" do
-
+  get 'square/:number' do
+    #that accepts a number and returns the square of that number. 
   end
+
+  get '/say/:number/:phrase' do
+    #accepts a number and a phrase and returns that phrase in a string the number of times given.
+    
+  end
+
+  
 
 end
