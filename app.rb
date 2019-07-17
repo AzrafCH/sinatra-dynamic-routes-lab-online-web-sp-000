908d063273c188fc0692646e9f6103ef06ba1035
Azraf Chowdhury <achowdhur@ltu.edu>
2019-07-17 03:58:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -28,6 +28,21 @@ class App < Sinatra::Base
 
   get '/:operation/:number1/:number2' do
     # that accepts an operation (add, subtract, multiply or divide) and performs the operation on the two numbers provided. For example, going to /add/1/2
+    num1 = params[:number1].to_i 
+    num2 = params[:number2].to_i
+
+    answer = 'Unable to perform this operation'
+
+    case params[:operation]
+    when "add"
+      answer = (num1 + num2).to_s
+    when "subtract"
+      answer = (num1 - num2).to_s 
+    when "multiply"
+      answer = (num1 * num2).to_s
+    when "divide"
+      answer = (num1 / num2).to_s
+    end 
   end
 
 
