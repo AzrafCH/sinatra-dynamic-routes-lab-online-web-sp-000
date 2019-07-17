34519899514d33aa53f618697bfc72649217b248
Azraf Chowdhury <achowdhur@ltu.edu>
2019-07-17 03:51:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
 
   get 'square/:number' do
     #that accepts a number and returns the square of that number.
-    (params[:number].to_i ** 2).to_s 
+    (params[:number].to_i ** 2).to_s
   end
 
   get '/say/:number/:phrase' do
