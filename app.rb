6af358bce6f8df51fa548623e30b864f2df2306b
Azraf Chowdhury <achowdhur@ltu.edu>
2019-07-17 03:35:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,14 +8,14 @@ class App < Sinatra::Base
   end
 
   get 'square/:number' do
-    #that accepts a number and returns the square of that number. 
+    #that accepts a number and returns the square of that number.
   end
 
   get '/say/:number/:phrase' do
     #accepts a number and a phrase and returns that phrase in a string the number of times given.
-    
+
   end
 
-  
+
 
 end
